4866874b595e15c63a34e8eb6648e6bdaf736c61
Mustafa Karakaş <mustafakarakas61ts862@gmail.com>
2022-08-07 11:44:08 +0300

    add adress

--- SwaggerConfig.java
+++ SwaggerConfig.java
@@ -11,6 +11,8 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+//http://localhost:8080/swagger-ui.html
+
 /* Add to pom.xml this dependencies
 <dependency>
 			<groupId>io.springfox</groupId>
