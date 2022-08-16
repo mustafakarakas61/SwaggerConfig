12a773f25f659ba76d56ab9872f94483a7badc22
Mustafa Karakaş <mustafakarakas61ts862@gmail.com>
2022-08-16 17:22:21 +0300

    Update SwaggerConfig.java

--- SwaggerConfig.java
+++ SwaggerConfig.java
@@ -12,7 +12,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.service.ApiInfo;
 
 //-------------------------------------------------------------------------URL
-//http://localhost:8080/swagger-ui.html //Ek Bilgi: Program Arguments: -Dserver.port=8081
+//http://localhost:8080/swagger-ui.html //Ek Bilgi: Program Arguments: -Dserver.port=8081 (Swagger'den bağımsız, sadece bilgi amaçlı)
 //http://localhost:8080/swagger-ui/#
 
 //-------------------------------------------------------------------------dependency
